576a6745968bd6e8e56f0a03c54b7918b91294cf
Chris Troutner <chris.troutner@gmail.com>
2020-03-29 19:03:47 -0700

    fix(slp): Fixed slp examples

--- applications/slp/lookup-token/lookup-token.js
+++ applications/slp/lookup-token/lookup-token.js
@@ -4,14 +4,14 @@
 
 // EDIT THIS WITH THE TOKEN ID YOU WANT TO LOOK UP
 const TOKENID =
-  '9f2b1b91a6ab0686d48e46345669889357b48dbfe8a5d817b857607d89693adc'
+  '682d6fd95e7a7612af49823bc44b3e396eb18f47a47926cb27984f342958f37e'
 
 // Set NETWORK to either testnet or mainnet
-const NETWORK = 'mainnet'
+const NETWORK = 'testnet'
 
 // REST API servers.
-const MAINNET_API = 'https://api.bchjs.cash/v3/'
-const TESTNET_API = 'http://tapi.bchjs.cash/v3/'
+const MAINNET_API = 'https://api.fullstack.cash/v3/'
+const TESTNET_API = 'http://tapi.fullstack.cash/v3/'
 
 // bch-js-examples require code from the main bch-js repo
 const BCHJS = require('@chris.troutner/bch-js')
