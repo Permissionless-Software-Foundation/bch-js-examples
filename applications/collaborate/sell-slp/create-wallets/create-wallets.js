f51d372f40092fcf455047316fdc7bdb558bbf2d
Chris Troutner <chris.troutner@gmail.com>
2020-08-19 12:26:22 -0700

    fix(collaborative txs): Refactoring the examples

--- applications/collaborate/sell-slp/create-wallets/create-wallets.js
+++ applications/collaborate/sell-slp/create-wallets/create-wallets.js
@@ -25,81 +25,64 @@ const fs = require('fs')
 const lang = 'english' // Set the language of the wallet.
 
 // These objects used for writing wallet information out to a file.
-let outStr = ''
+const outStr = ''
 const outObj = {}
 const outObj1 = {}
 
 async function createWallets () {
   try {
     // create 256 bit BIP39 mnemonic
-    const mnemonic = bchjs.Mnemonic.generate(
+    const sellerMnemonic = bchjs.Mnemonic.generate(
       128,
       bchjs.Mnemonic.wordLists()[lang]
     )
-    console.log('BIP44 $BCH Wallet')
-    outStr += 'BIP44 $BCH Wallet\n'
-    console.log(`128 bit ${lang} BIP39 Mnemonic: `, mnemonic)
-    outStr += `\n128 bit ${lang} BIP32 Mnemonic:\n${mnemonic}\n\n`
-    outObj.mnemonic = mnemonic
-    outObj1.mnemonic = mnemonic
+    const buyerMnemonic = bchjs.Mnemonic.generate(
+      128,
+      bchjs.Mnemonic.wordLists()[lang]
+    )
 
     // root seed buffer
-    const rootSeed = await bchjs.Mnemonic.toSeed(mnemonic)
+    const sellerRootSeed = await bchjs.Mnemonic.toSeed(sellerMnemonic)
+    const buyerRootSeed = await bchjs.Mnemonic.toSeed(buyerMnemonic)
 
     // master HDNode
-    let masterHDNode
-    if (NETWORK === 'mainnet') masterHDNode = bchjs.HDNode.fromSeed(rootSeed)
-    else masterHDNode = bchjs.HDNode.fromSeed(rootSeed, 'testnet') // Testnet
+    const sellerMasterHDNode = bchjs.HDNode.fromSeed(sellerRootSeed)
+    const buyerMasterHDNode = bchjs.HDNode.fromSeed(buyerRootSeed)
 
-    // HDNode of BIP44 account
-    console.log('BIP44 Account: "m/44\'/145\'/0\'"')
-    outStr += 'BIP44 Account: "m/44\'/145\'/0\'"\n'
+    const sellerChildNode = sellerMasterHDNode.derivePath("m/44'/145'/0'/0/0")
+    const buyerChildNode = buyerMasterHDNode.derivePath("m/44'/145'/0'/0/0")
 
-    // Generate the first 10 seed addresses.
-    for (let i = 0; i < 10; i++) {
-      const childNode = masterHDNode.derivePath(`m/44'/145'/0'/0/${i}`)
-      console.log(
-        `m/44'/145'/0'/0/${i}: ${bchjs.HDNode.toCashAddress(childNode)}`
-      )
-      outStr += `m/44'/145'/0'/0/${i}: ${bchjs.HDNode.toCashAddress(
-        childNode
-      )}\n`
+    const sellerObj = {}
+    sellerObj.cashAddress = bchjs.HDNode.toCashAddress(sellerChildNode)
+    sellerObj.legacyAddress = bchjs.HDNode.toLegacyAddress(sellerChildNode)
+    sellerObj.slpAddress = bchjs.SLP.HDNode.toSLPAddress(sellerChildNode)
+    sellerObj.WIF = bchjs.HDNode.toWIF(sellerChildNode)
 
-      // Save the first seed address for use in the .json output file.
-      if (i === 0) {
-        outObj.cashAddress = bchjs.HDNode.toCashAddress(childNode)
-        outObj.legacyAddress = bchjs.HDNode.toLegacyAddress(childNode)
-        outObj.slpAddress = bchjs.SLP.HDNode.toSLPAddress(childNode)
-        outObj.WIF = bchjs.HDNode.toWIF(childNode)
-      }
-
-      // Save the second seed address for use in the .json output file.
-      if (i === 1) {
-        outObj1.cashAddress = bchjs.HDNode.toCashAddress(childNode)
-        outObj1.legacyAddress = bchjs.HDNode.toLegacyAddress(childNode)
-        outObj1.slpAddress = bchjs.SLP.HDNode.toSLPAddress(childNode)
-        outObj1.WIF = bchjs.HDNode.toWIF(childNode)
-      }
-    }
-
-    // Write the extended wallet information into a text file.
-    fs.writeFile('wallet-info.txt', outStr, function (err) {
-      if (err) return console.error(err)
-
-      console.log('wallet-info.txt written successfully.')
-    })
+    const buyerObj = {}
+    buyerObj.cashAddress = bchjs.HDNode.toCashAddress(buyerChildNode)
+    buyerObj.legacyAddress = bchjs.HDNode.toLegacyAddress(buyerChildNode)
+    buyerObj.slpAddress = bchjs.SLP.HDNode.toSLPAddress(buyerChildNode)
+    buyerObj.WIF = bchjs.HDNode.toWIF(buyerChildNode)
 
     // Write out the basic information into a json file for other example apps to use.
-    fs.writeFile('wallet.json', JSON.stringify(outObj, null, 2), function (err) {
-      if (err) return console.error(err)
-      console.log('wallet.json written successfully.')
-    })
+    fs.writeFile(
+      'seller-wallet.json',
+      JSON.stringify(sellerObj, null, 2),
+      function (err) {
+        if (err) return console.error(err)
+        console.log('seller-wallet.json written successfully.')
+      }
+    )
 
     // Write out the basic information into a json file for other example apps to use.
-    fs.writeFile('wallet-1.json', JSON.stringify(outObj1, null, 2), function (err) {
-      if (err) return console.error(err)
-      console.log('wallet-1.json written successfully.')
-    })
+    fs.writeFile(
+      'buyer-wallet.json',
+      JSON.stringify(buyerObj, null, 2),
+      function (err) {
+        if (err) return console.error(err)
+        console.log('buyer-wallet.json written successfully.')
+      }
+    )
   } catch (err) {
     console.error('Error in createWallet(): ', err)
   }
